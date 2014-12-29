3e4fc692ce24327ba666cf519f401ec62ef46c03
Ken Robertson <ken@invalidlogic.com>
2014-12-28 20:05:51 -0800

    Update to use new theme

--- init.el
+++ init.el
@@ -21,7 +21,7 @@
 
 ;; set the theme
 (add-to-list 'custom-theme-load-path "~/.emacs.d/themes")
-(load-theme 'zenburn t nil)
+(load-theme 'monokai t nil)
 
 ;; cursor
 (setq-default cursor-type 'bar)
@@ -188,3 +188,17 @@
 (require 'server)
 (unless (server-running-p)
   (server-start))
+
+(custom-set-variables
+  ;; custom-set-variables was added by Custom.
+  ;; If you edit it by hand, you could mess it up, so be careful.
+  ;; Your init file should contain only one such instance.
+  ;; If there is more than one, they won't work right.
+  '(custom-safe-themes (quote ("c7471ce3bb42defac344b3ecfca74228731b5ab20f804fd1deb8e65dddeab26a" default)))
+  '(powerline-default-separator (quote arrow)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
