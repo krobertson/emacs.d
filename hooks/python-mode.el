7c69f4987b36c68fb42ad0424779ebb0caf48155
Ken Robertson <ken@invalidlogic.com>
2016-06-10 11:03:26 -0700

    Added python-mode settings

---
+++ hooks/python-mode.el
@@ -0,0 +1,5 @@
+;; set whitespace mode to show spaces and tabs as well
+(set (make-local-variable 'whitespace-style) '(face lines-tail tab-mark space-mark))
+
+;; enable whitespace mode
+(whitespace-mode)
